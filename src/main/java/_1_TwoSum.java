7db235753d96eaa52affd86d293409e68b83ebfc
zhangzy <zhangzhiyi@bonree.com>
2022-01-18 19:18:36 +0800

    desc

--- src/main/java/_1_TwoSum.java
+++ src/main/java/_1_TwoSum.java
@@ -2,12 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * description
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
  *
  * @author : zhangzy
  * @date : 2022/1/18
  */
-public class _1TwoSum {
+public class _1_TwoSum {
     public static void main(String[] args) {
         int[] nums = new int[]{12, 3, 5, 33, 2};
         int[] res = twoSum(nums, 5);
